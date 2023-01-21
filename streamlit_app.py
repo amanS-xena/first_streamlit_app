682d0bf267c785c2aeca2a0f40b519f17e3b08a5
Aman Saxena <88227543+amanS-xena@users.noreply.github.com>
2023-01-21 15:41:19 +0530

    Update streamlit_app.py
    resolving errors.

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ streamlit.header('🍉🍋BUILD YOUR OWN FRUIT SMOOTHIES🍍🍌');
 
 import pandas as pd;
 
-my_fruit_list = pf.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt');
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt');
 streamlit.dataframe(my_fruit_list);
