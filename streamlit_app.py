49a2100dfac65ff34e1c338c564159b3179ee84e
Aman Saxena <88227543+amanS-xena@users.noreply.github.com>
2023-01-21 15:49:24 +0530

    Update streamlit_app.py
    changed the index to a value.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ streamlit.header('🍉🍋BUILD YOUR OWN FRUIT SMOOTHIES🍍🍌');
 import pandas as pd;
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt');
+my_fruit_list = my_fruit_list.set_index('Fruit');
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
