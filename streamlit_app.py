82d27014bfac6fa254516a8ad2869a0c852e8bd4
Aman Saxena <88227543+amanS-xena@users.noreply.github.com>
2023-01-21 15:29:51 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit;
 streamlit.title('My Parents New Healthy Diner');
 streamlit.header('Breakfast Menu');
 streamlit.text('Omega 3 & Bluebery oatmeal');
-stramlit.text('Kale, Spinach and Rocket Smoothie');
+streamlit.text('Kale, Spinach and Rocket Smoothie');
 streamlit.text('Hard-boiled free-range eggs');
