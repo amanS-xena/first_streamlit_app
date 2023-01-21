3edf3f00f767d09c52cfb451b51848075bd2dc54
Aman Saxena <88227543+amanS-xena@users.noreply.github.com>
2023-01-21 15:27:37 +0530

    Update streamlit_app.py
    Added new header and title.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit;
 
 streamlit.title('My Parents New Healthy Diner');
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 & Bluebery oatmeal');
+stramlit.text('Kale, Spinach and Rocket Smoothie');
+streamlit.text('Hard-boiled free-range eggs');
