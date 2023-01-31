74256ae8e5e2d113d9d07ec65a9274da03b03c90
Aman Saxena <88227543+amanS-xena@users.noreply.github.com>
2023-01-31 23:27:53 +0530

    Update streamlit_app.py
    optimizing the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit;
+import pandas as pd;
+import requests;
+import snowflake.connector;
+from urllib.error import URLError
+
 
 streamlit.title('My mom new healthy dinner');
 streamlit.header('Breakfast Favourites');
@@ -10,7 +15,7 @@ streamlit.text('🥙Avocado toast');
 streamlit.header('🍉🍋BUILD YOUR OWN FRUIT SMOOTHIES🍍🍌');
 
 
-import pandas as pd;
+#import pandas as pd;
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt');
 my_fruit_list = my_fruit_list.set_index('Fruit');
@@ -24,7 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruit_selected]
 streamlit.dataframe(fruits_to_show);
 
 
-import requests
+#import requests
 
 
 #header to display streamlit API
@@ -44,7 +49,9 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # display the flattened data into a proper table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+#don't run anything past here
+streamlit.stop();
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
