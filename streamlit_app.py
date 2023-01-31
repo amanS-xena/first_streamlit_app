020b715fb1f8912c2581e42ba72e4948ab50db16
Aman Saxena <88227543+amanS-xena@users.noreply.github.com>
2023-01-31 23:59:03 +0530

    Update streamlit_app.py
    added a new function to load data

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,19 +70,33 @@ except URLError as e:
   
 
 #don't run anything past here
-streamlit.stop();
-#import snowflake.connector
+# streamlit.stop();
+# #import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT * from fruit_load_list")
+# my_data_row = my_cur.fetchall()
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_row)
 
 #Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add ?', 'jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+# add_my_fruit = streamlit.text_input('What fruit would you like to add ?', 'jackfruit')
+# streamlit.write('Thanks for adding ', add_my_fruit)
 
 
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+# my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+
+#Move the Fruit Load List Query and Load into a Button Action
+streamlit.header("The fruit load list contains:")
+#snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
+  
+#add a button to load the fruit
+if streanlit.button('Get fruit load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
